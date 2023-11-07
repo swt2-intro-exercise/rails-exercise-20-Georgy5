0f5a6062e8b5b032a0dfb2e82c3b2a4f97cb4c95
Karl Keller <kgk86@hotmail.com>
2023-11-07 19:42:31 +0100

    Generate Author model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_07_183708) do
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
